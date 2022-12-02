e014b7e1ba1d520c95565eaba9474c5c61709dfa
Tux <tux@localhost.localdomain>
2022-12-02 15:26:57 +0100

    test and set séparation

--- Projet_SI/Partie2/test-and-set.c
+++ Projet_SI/Partie2/test-and-set.c
@@ -1,26 +1,37 @@
-int main(int argc, char * argv[]){
-
-    int x,y;
-
     //première section instructions des assembleurs entre ""
     //deuxième section : spécification des des opérandes de sortie ds instructions
     //troisième section : contient les opérandes d'entrée
     //quatrième section : déclare les modifications apportées par les instructions
     //les sections sont séparées par :
+
+
+void init(){
     asm("lock:"                     
-            ".long 0"                    
-        "enter:"
+            ".long 0"  );
+}
+
+
+void lock(){
+    asm("enter:"
             "movl $1, %eax"              
             "xchgl %eax, (lock)"         
                             
                             
             "testl %eax, %eax"           
             "jnz enter"                  
-            "ret"
-        "leave:"
+            "ret");
+}
+
+void unlock(){
+    asm("leave:"
             "movl $0, %eax"              
             "xchgl %eax, (lock)"         
-            "ret"
-            :"=r"(y)
-            :"r"(x));
+            "ret");
+}
+
+
+
+int main(int argc, char * argv[]){
+
+
 }
