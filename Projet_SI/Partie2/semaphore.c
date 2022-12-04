1a62c16ab04bf199b9844619826c84b8c08f5f98
Albane <denisalbane@gmail.com>
2022-12-04 21:53:33 +0100

    modif semaphore

--- Projet_SI/Partie2/semaphore.c
+++ Projet_SI/Partie2/semaphore.c
@@ -5,16 +5,47 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <errno.h>
-#include <string.h>  
+#include <string.h> 
+#include "test-and-set.c"
 
-int my_sem_wait(){
+typedef struct my_sem {
+    int lock;
+    int val;
+}my_sem_t;
 
+int my_sem_init(my_sem_t* sem, int value){
+    sem = malloc(sizeof(my_sem_t));
+    if (sem == NULL){
+        return -1;
+    }
+    sem->val = value;
+    mutex_init(&sem->lock);
+    return 0;
 }
 
-int my_sem_init(){
-
+int my_sem_wait(my_sem_t* sem){
+    if(sem->val>0){
+        sem->val --;
+    }
+    else{
+        mutex_lock(&sem->lock);
+    }
+    return 0;
 }
 
-int my_sem_post(){
 
+int my_sem_post(my_sem_t* sem){
+    if (sem->val == 0){
+        sem->val ++;
+        mutex_unlock(&sem->lock);
+    }
+    else{
+        sem->val ++;
+    }
+    return 0;
+}
+
+int my_sem_destroy(my_sem_t* sem){
+    free(sem);
+    return 0;
 }
