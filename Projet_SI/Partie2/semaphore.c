29792fd092f62f3c6171ad2bab4b1a1c894348fe
Albane <denisalbane@gmail.com>
2022-12-04 17:21:44 +0100

    semaphore

---
+++ Projet_SI/Partie2/semaphore.c
@@ -0,0 +1,20 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <string.h>  
+
+int sem_wait(){
+    
+}
+
+int sem_init(){
+
+}
+
+int sem_post(){
+
+}
