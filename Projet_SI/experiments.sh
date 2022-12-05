2891c6495eec83a8f30e416b837c390b8c9107d1
Tux <tux@localhost.localdomain>
2022-12-05 18:58:57 +0100

    experiments.sh

--- Projet_SI/experiments.sh
+++ Projet_SI/experiments.sh
@@ -1,8 +1,40 @@
 #!/usr/bin/bash
 
-make -s cleantest
+
+make -s cleancsv 
 
 THREADS=(1 2 4 8 16 32 64)
+echo "thread,i,time" &>>mesuresProd.csv
+for thread in "${THREADS[@]}"; do
+    for i in {1..5}; do
+        make PC -s
+        /usr/bin/time -f "$thread,$i,%e" ./PC $thread  &>>mesuresProd.csv 
+        make clean -s
+    done
+done
+
+echo "thread,i,time" &>>mesuresPhilosophe.csv
+for thread in "${THREADS[@]}"; do
+    for i in {1..5}; do
+        make fec -s
+        /usr/bin/time -f "$thread,$i,%e" ./fec $thread  &>>mesuresPhilosophe.csv 
+        make clean -s
+    done
+done
+
+
+echo "thread,i,time" &>>mesuresEL.csv
+for thread in "${THREADS[@]}"; do
+    thread1=$((($thread+$thread%2)/2))
+    thread2=$((($thread-$thread%2)/2))
+    for i in {1..5}; do
+        make EL -s
+        /usr/bin/time -f "$thread,$i,%e" ./EL $thread1 $thread2 &>>mesuresEL.csv 
+        make clean -s
+    done
+done
+
+
 echo "thread,i,time" &>>mesuresTestAndSet.csv
 for thread in "${THREADS[@]}"; do
     for i in {1..5}; do
@@ -47,3 +79,6 @@ for thread in "${THREADS[@]}"; do
         make clean -s
     done
 done
+
+#./script.sh
+#bash script.sh
