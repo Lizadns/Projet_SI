3416f36d4aa02c353d8d2744c24379593a539d9b
Tux <tux@localhost.localdomain>
2022-12-05 19:46:43 +0100

    plot ajout

--- Projet_SI/plot.py
+++ Projet_SI/plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def moustache1(data_name, name):
+def moustache(data_name, name):
 
     data=pd.read_csv(data_name, sep = ',')
     data_time=data['time']
@@ -25,33 +25,14 @@ def moustache1(data_name, name):
     plt.savefig('../Projet_SI/Graphe/'+name+'.png')
     plt.close()
 
-def moustache(data_name, name):
-
-    data=pd.read_csv(data_name, sep = ',')
-    data_time=data['time']
-    data_thread=data['thread']
-
-    data_2=data_time[data_thread==2]
-    data_4=data_time[data_thread==4]
-    data_8=data_time[data_thread==8]
-    data_16=data_time[data_thread==16]
-    data_32=data_time[data_thread==32]
-    data_64=data_time[data_thread==64]
-
-    fig=plt.figure()
-    plt.boxplot([data_2,data_4,data_8,data_16,data_32,data_64], labels=[2,4,8,16,32,64],showmeans=True)
-    plt.ylabel('Temps [s]')
-    plt.ylim(bottom=0)
-    plt.xlabel('Nombre de thread')
-    plt.title('Boite à moustache de ' + name)
-    plt.savefig('../Projet_SI/Graphe/'+name+'.png')
-    plt.close()
-
-moustache1('mesuresEL.csv', "EcrivainsLecteurs")
+moustache('mesuresEL.csv', "EcrivainsLecteurs")
 moustache('mesuresPhilosophe.csv', "Philosophe")
-moustache1('mesuresProd.csv', "ProducteurConsommateur")
-moustache1('mesuresTestAndSet.csv', "Test-and-set")
-moustache1('mesuresTestAndTestAndSet.csv', "Test-and-test-and-set")
+moustache('mesuresProd.csv', "ProducteurConsommateur")
+moustache('mesuresTestAndSet.csv', "Test-and-set")
+moustache('mesuresTestAndTestAndSet.csv', "Test-and-test-and-set")
+moustache('mesuresEL2.csv', "EcrivainsLecteur avec attente active")
+moustache('mesuresPC2.csv', "ProducteurConsommateur avec attente active")
+moustache('mesuresPhilosophe2.csv', "Philosophe avec attente active")
 
 
 
