158cd40ecf1445e0e269ce45e80b0a4b22d7f7f4
Albane <denisalbane@gmail.com>
2022-12-06 20:44:32 +0100

    plot

--- Projet_SI/plot.py
+++ Projet_SI/plot.py
@@ -15,24 +15,34 @@ def moustache(data_name, name):
     data_16=data_time[data_thread==16]
     data_32=data_time[data_thread==32]
     data_64=data_time[data_thread==64]
-
+    mean_1=data_1.mean()
+    mean_2=data_2.mean()
+    mean_4=data_4.mean()
+    mean_8=data_8.mean()
+    mean_16=data_16.mean()
+    mean_32=data_32.mean()
+    mean_64=data_64.mean()
+    mean=[mean_1,mean_2,mean_4,mean_8,mean_16,mean_32,mean_64]
+    ecart=[np.std(data_1),np.std(data_2),np.std(data_4),np.std(data_8),np.std(data_16),np.std(data_32),np.std(data_64)]
     fig=plt.figure()
-    plt.boxplot([data_1,data_2,data_4,data_8,data_16,data_32,data_64], labels=[1,2,4,8,16,32,64],showmeans=True)
+    plt.plot([1,2,4,8,16,32,64],mean,marker = "o", linestyle= "--",markersize = 5, linewidth= 1, color= "royalblue")
+    plt.errorbar([1,2,4,8,16,32,64], mean,yerr=ecart,linestyle = "None",color= "g")
+    plt.legend(["moyenne", "ecart type"])
     plt.ylabel('Temps [s]')
     plt.grid(True)
     plt.ylim(bottom=0)
     plt.xlabel('Nombre de thread')
-    plt.title('Boite à moustache de ' + name)
+    plt.title('Mesures des performances de ' + name)
     plt.savefig('../Projet_SI/Graphe/'+name+'.png')
     plt.close()
 
 moustache('mesuresEL.csv', "EcrivainsLecteurs")
 moustache('mesuresPhilosophe.csv', "Philosophe")
-moustache('mesuresPC.csv', "ProducteurConsommateur")
+#moustache('mesuresPC.csv', "ProducteurConsommateur")
 moustache('mesuresTas.csv', "Test-and-set")
 moustache('mesuresTatas.csv', "Test-and-test-and-set")
 moustache('mesuresEL2.csv', "EcrivainsLecteur avec attente active")
-moustache('mesuresPC2.csv', "ProducteurConsommateur avec attente active")
+#moustache('mesuresPC2.csv', "ProducteurConsommateur avec attente active")
 moustache('mesuresPhilosophe2.csv', "Philosophe avec attente active")
 
 
