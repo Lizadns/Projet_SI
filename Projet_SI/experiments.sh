791d2fcfb58fb41c928383980e4abaa07d5b5191
Albane <denisalbane@gmail.com>
2022-12-07 09:07:11 +0100

    modifs

--- Projet_SI/experiments.sh
+++ Projet_SI/experiments.sh
@@ -3,26 +3,30 @@
 
 make -s cleancsv 
 
-THREADS=(1 2 4 8 16 32 64)
+#mesures performances Producteurs/consommateurs
+THREADS=(2 4 8 16 32 64)
 echo "thread,i,time" &>>mesuresProd.csv
 for thread in "${THREADS[@]}"; do
+    thread1=$(($thread/2))
     for i in {1..5}; do
         make PC -s
-        /usr/bin/time -f "$thread,$i,%e" ./PC $thread  &>>mesuresProd.csv 
+        /usr/bin/time -f "$thread,$i,%e" ./PC $thread1  &>>mesuresProd.csv 
         make clean -s
     done
 done
 
+#mesures performances Philosophe
+THREADS=(1 2 4 8 16 32 64)
 echo "thread,i,time" &>>mesuresPhilosophe.csv
 for thread in "${THREADS[@]}"; do
     for i in {1..5}; do
-        make fec -s
-        /usr/bin/time -f "$thread,$i,%e" ./fec $thread  &>>mesuresPhilosophe.csv 
+        make Philosophe -s
+        /usr/bin/time -f "$thread,$i,%e" ./Philosophe $thread  &>>mesuresPhilosophe.csv 
         make clean -s
     done
 done
 
-
+#mesures performances Ecrivains/lecteurs
 echo "thread,i,time" &>>mesuresEL.csv
 for thread in "${THREADS[@]}"; do
     thread1=$((($thread+$thread%2)/2))
@@ -34,7 +38,7 @@ for thread in "${THREADS[@]}"; do
     done
 done
 
-
+#mesures performances Test_and_set
 echo "thread,i,time" &>>mesuresTestAndSet.csv
 for thread in "${THREADS[@]}"; do
     for i in {1..5}; do
@@ -44,6 +48,7 @@ for thread in "${THREADS[@]}"; do
     done
 done
 
+#mesures performances test_and_test_and_set
 echo "thread,i,time" &>>mesuresTestAndTestAndSet.csv
 for thread in "${THREADS[@]}"; do
     for i in {1..5}; do
@@ -53,24 +58,30 @@ for thread in "${THREADS[@]}"; do
     done
 done
 
+#mesures performances Producteurs/consommateurs avec attente active
+THREADS=(2 4 8 16 32 64)
 echo "thread,i,time" &>>mesuresPC2.csv
 for thread in "${THREADS[@]}"; do
+    thread1=$(($thread/2))
     for i in {1..5}; do
         make PC2 -s
-        /usr/bin/time -f "$thread,$i,%e" ./PC2 $thread  &>>mesuresPC2.csv
+        /usr/bin/time -f "$thread,$i,%e" ./PC2 $thread1  &>>mesuresPC2.csv
         make clean -s
     done
 done
 
+#mesures performances Philosophe avec attente active
+THREADS=(1 2 4 8 16 32 64)
 echo "thread,i,time" &>>mesuresPhilosophe2.csv
 for thread in "${THREADS[@]}"; do
     for i in {1..5}; do
-        make fec2 -s
-        /usr/bin/time -f "$thread,$i,%e" ./fec2 $thread  &>>mesuresPhilosophe2.csv
+        make Philosophe2 -s
+        /usr/bin/time -f "$thread,$i,%e" ./Philosophe2 $thread  &>>mesuresPhilosophe2.csv
         make clean -s
     done
 done
 
+#mesures performances Ecrivains/lecteurs avec attente active
 echo "thread,i,time" &>>mesuresEL2.csv
 for thread in "${THREADS[@]}"; do
     thread1=$((($thread+$thread%2)/2))
