5dbf4a66f45d3f8eef57db01c47160ac18798415
Tux <tux@localhost.localdomain>
2022-12-07 09:01:31 +0100

    plot

--- Projet_SI/plot.py
+++ Projet_SI/plot.py
@@ -2,31 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def moustache(data_name, name):
+def plot(data_name, name):
 
     data=pd.read_csv(data_name, sep = ',')
     data_time=data['time']
     data_thread=data['thread']
+    thread=[1,2,4,8,16,32,64]
+    mean=[]
+    ecart=[]
+    for i in thread:
+        mean.append(data_time[data_thread==i].mean())
+        ecart.append(np.std(data_time[data_thread==i]))
 
-    data_1=data_time[data_thread==1]
-    data_2=data_time[data_thread==2]
-    data_4=data_time[data_thread==4]
-    data_8=data_time[data_thread==8]
-    data_16=data_time[data_thread==16]
-    data_32=data_time[data_thread==32]
-    data_64=data_time[data_thread==64]
-    mean_1=data_1.mean()
-    mean_2=data_2.mean()
-    mean_4=data_4.mean()
-    mean_8=data_8.mean()
-    mean_16=data_16.mean()
-    mean_32=data_32.mean()
-    mean_64=data_64.mean()
-    mean=[mean_1,mean_2,mean_4,mean_8,mean_16,mean_32,mean_64]
-    ecart=[np.std(data_1),np.std(data_2),np.std(data_4),np.std(data_8),np.std(data_16),np.std(data_32),np.std(data_64)]
     fig=plt.figure()
-    plt.plot([1,2,4,8,16,32,64],mean,marker = "o", linestyle= "--",markersize = 5, linewidth= 1, color= "royalblue")
-    plt.errorbar([1,2,4,8,16,32,64], mean,yerr=ecart,linestyle = "None",color= "g")
+    plt.plot(thread,mean,marker = "o", linestyle= "--",markersize = 5, linewidth= 1, color= "royalblue")
+    plt.errorbar(thread, mean,yerr=ecart,linestyle = "None",color= "g")
     plt.legend(["moyenne", "ecart type"])
     plt.ylabel('Temps [s]')
     plt.grid(True)
@@ -36,35 +26,51 @@ def moustache(data_name, name):
     plt.savefig('../Projet_SI/Graphe/'+name+'.png')
     plt.close()
 
-moustache('mesuresEL.csv', "EcrivainsLecteurs")
-moustache('mesuresPhilosophe.csv', "Philosophe")
-#moustache('mesuresPC.csv', "ProducteurConsommateur")
-moustache('mesuresTas.csv', "Test-and-set")
-moustache('mesuresTatas.csv', "Test-and-test-and-set")
-moustache('mesuresEL2.csv', "EcrivainsLecteur avec attente active")
-#moustache('mesuresPC2.csv', "ProducteurConsommateur avec attente active")
-moustache('mesuresPhilosophe2.csv', "Philosophe avec attente active")
 
+def compare(data_name1, name1, data_name2,name2):
 
+    data1=pd.read_csv(data_name1, sep = ',')
+    data_time1=data1['time']
+    data_thread1=data1['thread']
+    mean1=[]
+    ecart1=[]
 
-"""""
-mean_1=data_1.mean()
-mean_2=data_2.mean()
-mean_3=data_3.mean()
-mean_4=data_4.mean()
+    data2=pd.read_csv(data_name2, sep = ',')
+    data_time2=data2['time']
+    data_thread2=data2['thread']
+    mean2=[]
+    ecart2=[]
 
-fig1 = plt.figure()
-T=[mean_2,mean_3,mean_4]
+    thread=[1,2,4,8,16,32,64]
+    for i in thread:
+        mean1.append(data_time1[data_thread1==i].mean())
+        ecart1.append(np.std(data_time1[data_thread1==i]))
 
-M = [i for i in range(2,5)]
-plt.plot(M, T, color="blue", linewidth=1.0, linestyle="-")
+        mean2.append(data_time2[data_thread2==i].mean())
+        ecart2.append(np.std(data_time2[data_thread2==i]))
 
-plt.xlim(1,4)
-plt.xticks(np.linspace(1,4,4))
+    
+    fig=plt.figure()
+    plt.plot(thread,mean1,marker = "o", linestyle= "--",markersize = 5, linewidth= 1, color= "royalblue")
+    plt.errorbar(thread, mean1,yerr=ecart1,linestyle = "None",color= "g")
+    plt.plot(thread,mean2,marker = "o", linestyle= "--",markersize = 5, linewidth= 1, color= "black")
+    plt.errorbar(thread, mean2,yerr=ecart2,linestyle = "None",color= "m")
+    if(name1=="Test-and-set et Test-and-test-and-set"):
+        plt.legend(["moyenne de Test-and-set", "moyenne de Test-and-test-and-set","ecart type Test-and_set","ecart type Test-and-test-and-set"])
+    else:
+        plt.legend(["moyenne 1", "moyenne 2","ecart type 1","ecart type 2"])
+    plt.ylabel('Temps [s]')
+    plt.grid(True)
+    plt.ylim(bottom=0)
+    plt.xlabel('Nombre de thread')
+    plt.title('Comparaison des 2 performances de ' + name1)
+    plt.savefig('../Projet_SI/Graphe/'+name2+'.png')
+    plt.close()
 
-plt.ylim(0,30)
-plt.yticks(np.linspace(0,30,14))
-
-plt.show()
-plt.close()
-"""""
+plot('mesuresEL.csv', "EcrivainsLecteurs")
+plot('mesuresPhilosophe.csv', "Philosophe")
+plot('mesuresPC.csv', "ProducteurConsommateur")
+compare('mesuresTas.csv', "Test-and-set et Test-and-test-and-set",'mesuresTatas.csv', "Test-and-test-and-set")
+compare('mesuresEL.csv', "EcrivainsLecteurs",'mesuresEL2.csv', "EcrivainsLecteur avec attente active")
+compare('mesuresPC.csv', "ProducteurConsommateur",'mesuresPC2.csv', "ProducteurConsommateur avec attente active")
+compare('mesuresPhilosophe.csv', "Philosophe",'mesuresPhilosophe2.csv', "Philosophe avec attente active")
